016e0c4cc929835fb9726d4580cefd8f2aeb3f66
Sebastian <sebastian3270@gmail.com>
2021-02-02 11:28:35 +0100

    Creates Menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function showBurgerMenu() {
+    document.getElementById('popup-div-burger-menu').style.display = "block";
+ }
+
+ function closeBurgerMenu() {
+    document.getElementById('popup-div-burger-menu').style.display = "none";
+ }
+ 
