b60ca71f5e629345ae114d3da80f0405cc3f05ad
Sebastian <sebastian3270@gmail.com>
2021-02-04 12:08:22 +0100

    main part and section 1 finished

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function showBurgerMenu() {
     document.getElementById('popup-div-burger-menu').style.display = "none";
  }
  
+ function showFilterMenu() {
+   document.getElementById('popup-div-filter').style.display = "block";
+}
+
+function closeFilterMenu() {
+   document.getElementById('popup-div-filter').style.display = "none";
+}
