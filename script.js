2ffcf52ab9c1d398b616c551675bfa99fbb31488
Sebastian <sebastian3270@gmail.com>
2021-02-05 13:25:24 +0100

    Creates Footer

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function showBurgerMenu() {
 function closeFilterMenu() {
    document.getElementById('popup-div-filter').style.display = "none";
 }
+
+//accordion
+
+var acc = document.getElementsByClassName("accordion-button");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+   acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+      panel.style.display = "none";
+      } else {
+      panel.style.display = "block";
+      }
+   });
+}
